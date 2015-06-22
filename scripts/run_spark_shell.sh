39bb13145d79db934b8b2d198becd91f30e2bb85
Ankur Aggarwal <agankur21@gmail.com>
2015-06-22 20:34:33 +0530

    Added script for spark shell

---
+++ scripts/run_spark_shell.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+#set -x
+
+##############################################################################
+#
+# Script to run Spark Scala job.
+#
+##############################################################################
+export PROJECT_HOME=/mnt/git-repo/DecisiveCMS
+source "${PROJECT_HOME}/scripts/common_func.sh";
+SPARK_HOME="/usr/local/spark";
+JOB_CLASS="com.scoopwhoop.dcms.RunApplication";
+JOB_JAR="${PROJECT_HOME}/target/scala-2.10/dcms-assembly.jar"
+JOB_DEP_JARS="${PROJECT_HOME}/lib/joda-time-2.7.jar,${PROJECT_HOME}/lib/joda-convert-1.7.jar"
+MASTER="local[4]"
+##############################################################################
+
+
+##############################################################################
+#
+# Function MAIN()
+#
+##############################################################################
+function MAIN()
+{
+   loggerInfo "***      Script: $0   START  ***";
+  
+   loggerInfo "Running Spark shell with Jar : ${JOB_JAR} "
+   ${SPARK_HOME}/bin/spark-shell --master ${MASTER}  --jars ${JOB_JAR} --num-executors 5 --executor-memory 2g --driver-memory 1g  ;
+   loggerInfo "***      Script: $0    END   ***";
+}
+##############################################################################
+
+
+##############################################################################
+#
+# Script Entry
+#
+##############################################################################
+MAIN $@;
+
+
