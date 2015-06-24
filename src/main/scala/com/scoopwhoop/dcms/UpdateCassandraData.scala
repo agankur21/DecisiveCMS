6d9151776f30d4be8c8d7eb9b1447d11b8242032
Ankur Aggarwal <agankur21@gmail.com>
2015-06-24 20:18:07 +0530

    Removed conversion to String and made variables of type Any

--- src/main/scala/com/scoopwhoop/dcms/UpdateCassandraData.scala
+++ src/main/scala/com/scoopwhoop/dcms/UpdateCassandraData.scala
@@ -12,14 +12,14 @@ class UpdateCassandraData extends Serializable  {
     val jsonFields = eventFields.map(x =>x.split(",")(0))
     val tableEventFieldsIndexMap  = eventFields.map(x =>x.split(",")(1)).zipWithIndex.toMap
 
-    case class Users(user_id:String,browser : String,browser_version:String,region:String,city:String,country_code:String,
-                     os: String,device:String) extends Serializable
+    case class Users(user_id:Any,browser : Any,browser_version:Any,region:Any,city:Any,country_code:Any,
+                     os: Any,device:Any) extends Serializable
 
-    case class Pages(url:String,title:String,category:String,author:String,screen_height:String, screen_width:String) extends Serializable
+    case class Pages(url:Any,title:Any,category:Any,author:Any,screen_height:Any, screen_width:Any) extends Serializable
 
-    case class Events(url:String,user_id:String,event:String,time:String,title:String,category:String,author:String,
-                      screen_height:String,screen_width:String,from_url:String,event_destination:String,screen_location:String,
-                      search_engine:String,mp_keyword:String,mp_lib:String,lib_version:String,user_data:UDTValue,
+    case class Events(url:Any,user_id:Any,event:Any,time:Any,title:Any,category:Any,author:Any,
+                      screen_height:Any,screen_width:Any,from_url:Any,event_destination:Any,screen_location:Any,
+                      search_engine:Any,mp_keyword:Any,mp_lib:Any,lib_version:Any,user_data:UDTValue,
                       referrer_data:UDTValue,utm_data:UDTValue) extends Serializable
 
 
@@ -27,44 +27,46 @@ class UpdateCassandraData extends Serializable  {
         val sqlContext = new org.apache.spark.sql.SQLContext(sparkContext)
         return  sqlContext.read.json(path)
     }
-
+  
     def eventMapper(row:Row):Events={
         val tableEventFieldsIndexMap_ = this.tableEventFieldsIndexMap
-        val user_data = UDTValue.fromMap(Map("browser" -> row(tableEventFieldsIndexMap_("browser")).toString,
-            "browser_version" -> row(tableEventFieldsIndexMap_("browser_version")).toString,
-            "region" -> row(tableEventFieldsIndexMap_("region")).toString,
-            "city" -> row(tableEventFieldsIndexMap_("city")).toString,
-            "country_code" -> row(tableEventFieldsIndexMap_("country_code")).toString,
-            "os" -> row(tableEventFieldsIndexMap_("os")).toString,
-            "device" -> row(tableEventFieldsIndexMap_("device")).toString,
-            "device_type" -> row(tableEventFieldsIndexMap_("device_type")).toString))
-        val referrer_data = UDTValue.fromMap(Map("initial_referrer" -> row(tableEventFieldsIndexMap_("initial_referrer")).toString,
-            "initial_referring_domain" -> row(tableEventFieldsIndexMap_("initial_referring_domain")).toString,
-            "referrer" -> row(tableEventFieldsIndexMap_("referrer")).toString,
-            "referring_domain" -> row(tableEventFieldsIndexMap_("referring_domain")).toString))
-        val utm_data = UDTValue.fromMap(Map("utm_campaign" -> row(tableEventFieldsIndexMap_("utm_campaign")).toString,
-            "utm_content" -> row(tableEventFieldsIndexMap_("utm_content")).toString,
-            "utm_medium" -> row(tableEventFieldsIndexMap_("utm_medium")).toString,
-            "utm_source" -> row(tableEventFieldsIndexMap_("utm_medium")).toString))
-        val eventRow = Events(row(tableEventFieldsIndexMap_("url")).toString,row(tableEventFieldsIndexMap_("user_id")).toString,row(tableEventFieldsIndexMap_("event")).toString,
-            row(tableEventFieldsIndexMap_("time")).toString,row(tableEventFieldsIndexMap_("title")).toString,row(tableEventFieldsIndexMap_("category")).toString,
-            row(tableEventFieldsIndexMap_("author")).toString,row(tableEventFieldsIndexMap_("screen_height")).toString,row(tableEventFieldsIndexMap_("screen_width")).toString,
-            row(tableEventFieldsIndexMap_("from_url")).toString,row(tableEventFieldsIndexMap_("event_destination")).toString,row(tableEventFieldsIndexMap_("screen_location")).toString,
-            row(tableEventFieldsIndexMap_("search_engine")).toString,row(tableEventFieldsIndexMap_("mp_keyword")).toString,row(tableEventFieldsIndexMap("mp_lib")).toString,
-            row(tableEventFieldsIndexMap_("lib_version")).toString,user_data,referrer_data,utm_data)
+        val user_data = UDTValue.fromMap(Map("browser" -> row(tableEventFieldsIndexMap_("browser")),
+            "browser_version" -> row(tableEventFieldsIndexMap_("browser_version")),
+            "region" -> row(tableEventFieldsIndexMap_("region")),
+            "city" -> row(tableEventFieldsIndexMap_("city")),
+            "country_code" -> row(tableEventFieldsIndexMap_("country_code")),
+            "os" -> row(tableEventFieldsIndexMap_("os")),
+            "device" -> row(tableEventFieldsIndexMap_("device")),
+            "device_type" -> row(tableEventFieldsIndexMap_("device_type"))))
+        val referrer_data = UDTValue.fromMap(Map("initial_referrer" -> row(tableEventFieldsIndexMap_("initial_referrer")),
+            "initial_referring_domain" -> row(tableEventFieldsIndexMap_("initial_referring_domain")),
+            "referrer" -> row(tableEventFieldsIndexMap_("referrer")),
+            "referring_domain" -> row(tableEventFieldsIndexMap_("referring_domain"))))
+        val utm_data = UDTValue.fromMap(Map("utm_campaign" -> row(tableEventFieldsIndexMap_("utm_campaign")),
+            "utm_content" -> row(tableEventFieldsIndexMap_("utm_content")),
+            "utm_medium" -> row(tableEventFieldsIndexMap_("utm_medium")),
+            "utm_source" -> row(tableEventFieldsIndexMap_("utm_medium"))))
+        val eventRow = Events(row(tableEventFieldsIndexMap_("url")),row(tableEventFieldsIndexMap_("user_id")),row(tableEventFieldsIndexMap_("event")),
+            row(tableEventFieldsIndexMap_("time")),row(tableEventFieldsIndexMap_("title")),row(tableEventFieldsIndexMap_("category")),
+            row(tableEventFieldsIndexMap_("author")),row(tableEventFieldsIndexMap_("screen_height")),row(tableEventFieldsIndexMap_("screen_width")),
+            row(tableEventFieldsIndexMap_("from_url")),row(tableEventFieldsIndexMap_("event_destination")),row(tableEventFieldsIndexMap_("screen_location")),
+            row(tableEventFieldsIndexMap_("search_engine")),row(tableEventFieldsIndexMap_("mp_keyword")),row(tableEventFieldsIndexMap("mp_lib")),
+            row(tableEventFieldsIndexMap_("lib_version")),user_data,referrer_data,utm_data)
         return eventRow;
     }
 
     
     def updateEventsData(eventData: DataFrame,keySpace:String,table:String):Unit = {
-
         val events = eventData.select(jsonFields(0),jsonFields(1),jsonFields(2),jsonFields(3),jsonFields(4),jsonFields(5),
             jsonFields(6),jsonFields(7),jsonFields(8),jsonFields(9),jsonFields(10),jsonFields(11),jsonFields(12),
             jsonFields(13),jsonFields(14),jsonFields(15),jsonFields(16),jsonFields(17),jsonFields(18),jsonFields(19),
             jsonFields(20),jsonFields(21),jsonFields(22),jsonFields(23),jsonFields(24),jsonFields(25),jsonFields(26),
             jsonFields(27),jsonFields(28),jsonFields(29),jsonFields(30),jsonFields(31))
-        events.map(eventMapper).saveToCassandra(keySpace, table)
+        events.map(eventMapper).saveToCassandra(keySpace, table,SomeColumns("url","user_id","event","time","title","category",
+            "author", "screen_height","screen_width","from_url","event_destination","screen_location","search_engine",
+            "mp_keyword","mp_lib","lib_version","user_data","referrer_data","utm_data"))
     }
+
 
     def updateUsersData(sparkContext: SparkContext,keySpace:String,table:String):Unit = {
         val userTable = sparkContext.cassandraTable[Users](keySpace,"events").select("user_id","browser",
