e1082d405a3fa5323591476373999ffafdb1d8a0
Ankur Aggarwal <agankur21@gmail.com>
2015-08-05 22:18:54 +0530

    Some changes

--- source/scala/src/main/scala/com/scoopwhoop/recommender/ItemItemSimilarity.scala
+++ source/scala/src/main/scala/com/scoopwhoop/recommender/ItemItemSimilarity.scala
@@ -1,17 +1,21 @@
 package com.scoopwhoop.recommender
 
-import org.apache.spark.SparkContext
 import org.apache.spark.rdd.RDD
-import org.apache.spark.mllib.linalg.distributed.{CoordinateMatrix, MatrixEntry}
+import org.apache.spark.mllib.linalg.distributed.{RowMatrix,MatrixEntry}
+import org.apache.spark.mllib.linalg.{Vectors}
+import com.scoopwhoop.logger.Logger
 
 
 class ItemItemSimilarity extends RecommenderAlgorithmModule {
-
-    private def convertStringToID(stringElement: String, map: scala.collection.Map[String, Long]): Long = {
+    
+    var userIdMap = scala.collection.Map[String, Long]()
+    var itemIdMap  = scala.collection.Map[String, Long]()
+    var idItemMap = scala.collection.Map[Long, String]()
+    def convertStringToID(stringElement: String, map: scala.collection.Map[String, Long]): Long = {
         map.getOrElse(stringElement, -1)
     }
 
-    private def convertIDToString(id: Long, map: scala.collection.Map[Long, String]): String = {
+    def convertIDToString(id: Long, map: scala.collection.Map[Long, String]): String = {
         map.getOrElse(id, "")
     }
 
@@ -21,20 +25,31 @@ class ItemItemSimilarity extends RecommenderAlgorithmModule {
       * @return An RDD for (String,String,Float)
       */
     def calculatePredictedRating(ratings: RDD[(String, String, Float)]): RDD[(String, String, Float)] = {
-        val sparkContext = ratings.sparkContext
-        val userIdMapBroad = sparkContext.broadcast(ratings.map(_._1).distinct.zipWithIndex.collectAsMap())
-        val itemIdMapBroad = sparkContext.broadcast(ratings.map(_._2).distinct.zipWithIndex.collectAsMap())
-        val idItemMapBroad = sparkContext.broadcast(itemIdMapBroad.value.map(x => x._2 -> x._1))
-        val userItemRating = ratings.map { case (userId: String, itemId: String, rating: Float) =>
-            new MatrixEntry(convertStringToID(userId, userIdMapBroad.value),
-                convertStringToID(itemId, itemIdMapBroad.value), rating.toDouble)
+        ratings.persist()
+        this.userIdMap = ratings.map(_._1).distinct.zipWithIndex.collectAsMap()
+        this.itemIdMap = ratings.map(_._2).distinct.zipWithIndex.collectAsMap()
+        this.idItemMap = this.itemIdMap.map(x => x._2 -> x._1)
+        val userIdItemIdRating = ratings.map { case (userId: String, itemId: String, rating: Float) =>
+            (convertStringToID(userId, this.userIdMap),
+                convertStringToID(itemId, this.itemIdMap), rating.toDouble)
         }
-        userItemRating.cache()
-        val ratingCoordinateMatrix = new CoordinateMatrix(userItemRating)
-        val ratingRowMatrix = ratingCoordinateMatrix.toRowMatrix()
+        val numItems = this.itemIdMap.size
+        val numUsers = this.userIdMap.size
+        val userVectors = userIdItemIdRating.groupBy(_._1)
+                        .filter {case(userId:Long,itemSeq : Iterable[(Long,Long,Double)]) => itemSeq.size > 0 }
+                        .map{ case(userId:Long,itemSeq : Iterable[(Long,Long,Double)]) => Vectors.sparse(numItems,itemSeq.map(x => (x._2.toInt,x._3)).toSeq)  }
+        userVectors.persist()
+        ratings.unpersist()
+        val numberOfEntriesInFirstRow = userVectors.first().size
+        val numberOfRows = userVectors.count()
+        Logger.logInfo(s"Columns in first row : $numberOfEntriesInFirstRow")
+        Logger.logInfo(s"Number of entries in the user vectors: $numberOfRows")
+        Logger.logInfo(s"Total number of users : $numUsers")
+        Logger.logInfo(s"Total number of items : $numItems")
+        val ratingRowMatrix = new RowMatrix(userVectors,numUsers,numItems)
         val itemItemCoordinateMatrix = ratingRowMatrix.columnSimilarities(0.3)
         val similarityEntries = itemItemCoordinateMatrix.entries.distinct.map { case (x: MatrixEntry) =>
-            (convertIDToString(x.i, idItemMapBroad.value), convertIDToString(x.j, idItemMapBroad.value), x.value.toFloat)
+            (convertIDToString(x.i, this.idItemMap), convertIDToString(x.j, this.idItemMap), x.value.toFloat)
         }
         return similarityEntries;
     }
