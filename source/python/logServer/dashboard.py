024e49f240970d2254b6b4aea3886e6fc15c16db
Rohit Gupta <rohit221991@iitj.ac.in>
2015-08-24 18:45:35 +0530

    adding kaafka producer code and removing unused code

--- source/python/logServer/dashboard.py
+++ source/python/logServer/dashboard.py
@@ -1,14 +1,8 @@
 import web
 import os
-import json
-from models import Logs , IP ,UserAgent ,UnresolvedIP
-from cassandra.cqlengine.management import sync_table
-from cassandra.cqlengine import connection
-from jinja2 import Environment , FileSystemLoader
-import time_uuid
-import time
-import utils
-from cassandra.cqlengine.query import DoesNotExist
+from jinja2 import Environment,FileSystemLoader
+from kafka import SimpleProducer, KafkaClient
+
 
 
 urls = (
@@ -21,18 +15,8 @@ urls = (
 app = web.application(urls, globals())
 
 
-
-def _connect_to_cassandra(keyspace):
-    connection.setup(['127.0.0.1'], keyspace, protocol_version=3)
-    sync_table(Logs)
-    sync_table(IP)
-    sync_table(UserAgent)
-    sync_table(UnresolvedIP)
-
-    return
-
-
-_connect_to_cassandra('logapp')
+kafka = KafkaClient('localhost:9092')
+producer = SimpleProducer(kafka)
 
 
 def render_template(template_name, **context):
@@ -50,61 +34,8 @@ def render_template(template_name, **context):
 
 class EventCapture:
     def POST(self):
-        """
-        Insert some log data into Cassandra.
-        The log data is encoded as a JSON string.
-        """
         value = web.data()
-        value_parsed = json.loads(value)
-
-        product_id = str(value_parsed.get('product_id', ""))
-        ip = str(value_parsed.get('ip', ""))
-        time = utils.getTimeStampFromTimeString(str(value_parsed.get('time', "2015 18 7 17 37")))
-        page_id = str(value_parsed.get('page_id', ""))
-        cookie_id = str(value_parsed.get('cookie_id', ""))
-        user_agent = str(value_parsed.get('user_agent', ""))
-        author = str(value_parsed.get('author'))
-        category = str(value_parsed.get('category'))
-        date = utils.getDayFromTimeStamp(time)
-
-        event_where = str(value_parsed['event_properties']['where'])
-        event_desc = str(value_parsed['event_properties']['desc'])
-
-        referer_url = str(value_parsed['referer_info']['url'])
-        referer_domain = str(value_parsed['referer_info']['domain'])
-
-
-        # saving log into database
-
-        log = Logs(product_id=product_id, ip=ip,date=date,time=time, page_id=page_id, cookie_id=cookie_id,
-                          user_agent=user_agent, author=author, category=category, event_where=event_where,
-                          event_desc=event_desc, referer_domain=referer_domain, referer_url=referer_url)
-        log.save()
-
-
-        # saving IP info into database
-        ip_filter = IP.filter(ip=ip)
-        ip_obj = ip_filter.first()
-        if (ip_obj is None):
-            location_info = utils.getLocationInfo(ip)
-            if location_info is not None:
-                (region, city, country, latitude, longitude) = location_info
-                ip_obj =IP(ip=ip,region=region, city=city, country=country, latitude=latitude, longitude=longitude)
-                ip_obj.save()
-            else:
-                ip_obj =UnresolvedIP(ip=ip)
-                ip_obj.save()
-
-
-
-        #saving useragent into database
-        ua_filter = UserAgent.filter(ua=user_agent)
-        ua_obj = ua_filter.first()
-        if (ua_obj is None):
-            (browser, browser_version, os, os_version, device, device_type) = utils.getDeviceInfo(user_agent)
-            ua_obj = UserAgent(ua=user_agent,browser=browser, browser_version=browser_version, os=os, os_version=os_version,
-                                      device=device, device_type=device_type)
-            ua_obj.save()
+        producer.send_messages('logs',str(value) )
 
 
 class index:
